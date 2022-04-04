1c5739040d41619763a1919c438e17d8634cd7af
Ben Shek <shek798@gmail.com>
2022-04-04 09:16:26 +0000

    Home page adjustment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import Logo from "../assets/dog.png";
 import ReorderIcon from '@mui/icons-material/Reorder';
 import '../styles/NavBar.css';
 
 function NavBar() {
+
+  const [openLinks, setOpenLinks] = useState(false)
+  const toggleNavBar = () => {
+    setOpenLinks(!openLinks);
+  }
+  
   return (
     <div className = "navbar">
-      <div className = "leftSide">
-        <img src={Logo} />
+      <div className = "leftSide" id = {openLinks ? "open" : "close"}>
+        <img src = {Logo} />
+        <div className = "hiddenLinks">
+          <Link to = "/"> Home </Link>
+          <Link to = "/about"> About </Link>
+          <Link to = "/doglist"> Dog List </Link>
+          <Link to = "/contact"> Contact </Link>
+          <Link to = "/workerlogin"> Worker Login </Link>
+          <Link to = "/workerregister"> Worker Register </Link>
+        </div>
       </div>
       <div className = "rightSide">
         <Link to = "/"> Home </Link>
@@ -17,7 +31,7 @@ function NavBar() {
         <Link to = "/contact"> Contact </Link>
         <Link to = "/workerlogin"> Worker Login </Link>
         <Link to = "/workerregister"> Worker Register </Link>
-        <button>
+        <button onClick = {toggleNavBar}>
           <ReorderIcon />
         </button>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Home.css';
+import bgImage from "../assets/dogbg.jpg"
 
 function Home() {
   return (
-    <div>
+    <div className = "home" style = {{ backgroundImage: `url(${bgImage})`}}>
+      <div className = "homeHeader">
+        <h1>The Canine Shelter</h1>
+        <p>Report dogs in need of help or help them find a permanent home</p>
+        <Link to = "/contact">
+          <button>CONTACT US</button>
+        </Link>
+        
+      </div>
       Testing 1234
     </div>
   )
