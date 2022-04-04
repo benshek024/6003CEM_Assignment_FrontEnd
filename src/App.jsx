f928a88c1dd1e4c28573dd7c5120ad2984f82bc2
Ben Shek <shek798@gmail.com>
2022-04-04 11:19:40 +0000

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from "./components/NavBar";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Routes>
             <Route path='/' exact element={<Home />} />
           </Routes>
+         <Footer />
        </Router>
       </div>
     </main>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import '../styles/Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TwitterIcon /> <FacebookIcon />
+      </div>
+      
+      <a href="https://www.freepik.com/vectors/watercolor-dog">Watercolor dog vector created by rawpixel.com - www.freepik.com</a>
+      <a href="https://www.jamiesale-cartoonist.com/free-cartoon-dog-vector-clip-art/">Dog vector created by Jamie Sale</a>
+    </div>
+  );
+}
+
+export default Footer;
